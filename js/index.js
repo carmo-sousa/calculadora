898b410286e96c832f2da3fe592cebfce6871f58
Carmo-sousa <carmosousa20@outlook.com>
2020-04-07 20:51:19 -0400

    Layout finalizado.

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll(".buttons");
+
+const buttonsArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+buttons.forEach((button) => {});
+
+// console.log(buttons[1].innerHTML);
