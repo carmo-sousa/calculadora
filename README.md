d5df15a5d3740b0645ec6f02db8c48df8b399f2e
Carmo-sousa <carmosousa20@outlook.com>
2020-04-14 18:05:15 -0400

    Adicionando função para inserção dos parenteses.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-#Calculadora
+# Calculadora
+
+## Passos para colocar parenteses
+
+```
+se parenteses in string:
+    se ultimo parenteses igual a (:
+        se número depois do parenteses:
+            se operador depois do número:
+                adicionar (
+            se não:
+                adicionar )
+        se não:
+            não fazer nada
+    se ultimo parenteses igual a ):
+        adicionar um operador qualquer e depois o parentes (
+se não tiver parenteses e a string for vazia:
+    adicionar o parenteses (
+```
--- js/index.js
+++ js/index.js
@@ -6,21 +6,22 @@ const btnMinus = document.querySelector('#btn-minus');
 const btnMultiply = document.querySelector('#btn-multiply');
 const btnDivide = document.querySelector('#btn-divide');
 const btnPercent = document.querySelector('#btn-percent');
+const btnParenthese = document.querySelector('#btn-parenthese');
 
 var display = document.querySelector('#display');
 
 const buttonsArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const operatorsArray = ['+', '-', 'x', '/', '%'];
 
+btnParenthese.addEventListener('click', () => parentheses(display));
+
 btnClear.addEventListener('click', () => clear(display));
-btnDelete.addEventListener('click', function () {
-  deleteOne(display);
-});
+btnDelete.addEventListener('click', () => deleteOne(display));
 
 buttons.forEach(button => {
   if (buttonsArray.indexOf(button.innerHTML.trim(), 0) >= 0) {
     button.addEventListener('click', function () {
-      setValue(display, this);
+      setValue(display, this.innerHTML);
     });
   } else if (operatorsArray.indexOf(button.innerHTML.trim(), 0) >= 0) {
     button.addEventListener('click', function () {
@@ -30,7 +31,7 @@ buttons.forEach(button => {
 });
 
 function setValue(display, value) {
-  display.innerHTML += value.innerHTML;
+  display.innerHTML += value;
 }
 
 function clear(display) {
@@ -38,10 +39,11 @@ function clear(display) {
 }
 
 function setOperators(display, value) {
-  let re = /(\+$|\-$|\x$|\/$|\%$|\%$)/g;
+  let re = /\+$|\-$|\x$|\/$/g;
+  let content = display.innerHTML.trim();
 
-  if (display.innerHTML.search(re) < 0 && display.innerHTML.length != 0) {
-    display.innerHTML += value.innerHTML;
+  if (content.search(re) < 0 && content.length != 0) {
+    setValue(display, value.innerHTML.trim());
   }
 }
 
@@ -52,3 +54,41 @@ function deleteOne(display) {
 }
 
 function percent(value, display) {}
+
+function parentheses(display) {
+  let re = /\+$|\-$|\x$|\/$/g;
+  let value = display.innerHTML.trim();
+  let array = value.match(/([\(*\)*])/g);
+
+  if (value.length === 0 || re.test(value)) {
+    setValue(display, '(');
+  } else if (/[0-9%]+$/g.test(value) && !re.test(value) && array[array.length - 1] != ')') {
+    setValue(display, ')');
+  }
+}
+
+/*
+  let re = /([\(*\)*])/g;
+  let control = /\+$|\-$|\x$|\/$/g;
+  let value = display.innerHTML;
+  let array = value.match(re);
+  console.log(array);
+
+  if (re.test(value)) {
+    if (array[array.length - 1] == '(') {
+      if (value.search(/\([0-9]+/g) >= 0) {
+        if (control.test(value)) {
+          setValue(display, '(');
+        }
+      }
+    } else if (array[array.length - 1] == ')') {
+      if (/\)$/g.test(value)) {
+        setValue(display, 'x(');
+      } else if (/[0-9]+$/g.test(value)) {
+        setValue(display, ')');
+      }
+    }
+  } else if (!re.test(value) && value.length == 0) {
+    setValue(display, '(');
+  }
+*/
